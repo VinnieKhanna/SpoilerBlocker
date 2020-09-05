b9d24549f184e4091530ec79cbaf831a04d28dd4
rajbsrivastava <39221322+rajbsrivastava@users.noreply.github.com>
2020-09-05 16:25:39 -0400

    Replaces text with other text

--- content.js
+++ content.js
@@ -3,4 +3,12 @@
 //     "matches": ["http://*/*", "https://*/*"]
 //   }]
 
-document.body.innerHTML = ' ';
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i].innerHTML.includes('Harry Potter')) {
+        text[i].innerHTML = text[i].innerHTML.replace('Harry Potter', 'Hairy Potter is very bad')
+    }
+}
+
+//document.body.innerHTML = ' ';
