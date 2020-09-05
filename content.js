67b4df98a6bead30a076a78cba7acea037b3c36a
rajbsrivastava <39221322+rajbsrivastava@users.noreply.github.com>
2020-09-05 18:51:30 -0400

    Added highlighting text functionality

--- content.js
+++ content.js
@@ -5,8 +5,16 @@
 
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
 
+//Replaces text
 for (let i = 0; i < text.length; i++) {
     if (text[i].innerHTML.includes(config)) {
         text[i].innerHTML = text[i].innerHTML.replace('[REDACTED]')
     }
 }
+
+//Highlights text
+/*for (let i = 0; i < text.length; i++) {
+    if (text[i].innerHTML.includes('Harry Potter')) {
+        text[i].innerHTML = text[i].innerHTML.replace('Harry Potter', (match) => '<span style="color: #000000; background-color: #000000">Harry Potter</span>')
+    }
+}*/
