913ba42df76d583ca0d4b6bd46553b35d5c9c622
mpurushothapu3 <55211137+mpurushothapu3@users.noreply.github.com>
2020-09-05 19:38:16 -0400

    Added multiline in regex.

--- content.js
+++ content.js
@@ -6,7 +6,7 @@
 searchText2 = config;
 
 text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
-regex = new RegExp(searchText2, 'gi')
+regex = new RegExp(searchText2, 'gim')
 for (let i = 0; i < text.length; i++) {
     if (text[i].innerHTML.includes(searchText2)) {
         text[i].innerHTML = text[i].innerHTML.replace(regex, '<mark style="background-color>'+searchText2+'</mark>')
