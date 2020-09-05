514bb5f5ada9fb69e99f11322a45574ba649248b
Raj Srivastava <rsrivastava60@gatech.edu>
2020-09-05 16:43:01 -0400

    Trying spoiler highlighting

--- content.js
+++ content.js
@@ -5,9 +5,19 @@
 
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
 
-for (let i = 0; i < text.length; i++) {
+/*for (let i = 0; i < text.length; i++) {
     if (text[i].innerHTML.includes('Harry Potter')) {
         text[i].innerHTML = text[i].innerHTML.replace('Harry Potter', 'Hairy Potter is very bad')
+    }
+}*/
+for (let i = 0; i < text.length; i++) {
+    if (text[i].innerHTML.includes('Harry Potter')) {
+        let innerHTML = text[i].innerHTML;
+        let index = innerHTML.indexOf("Harry Potter");
+        if (index >= 0) {
+            innerHTML = innerHTML.substring(0,index) + "<span class='spoiler'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+            text[i].innerHTML = innerHTML;
+        }
     }
 }
 
