50f53de387998d287f80e4a5a3288ce007379371
Raj Srivastava <rsrivastava60@gatech.edu>
2020-09-05 18:48:24 -0400

    Highlights Harry Potter instead of highlighting

--- content.js
+++ content.js
@@ -3,22 +3,23 @@
 //     "matches": ["http://*/*", "https://*/*"]
 //   }]
 
+//const term = "Harry Potter"; // search query we want to highlight in results
+//const results; // search results
+
+//text.replace(new RegExp(term, "gi"), (match) => `<mark>${match}</mark>`);
+
 const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i].innerHTML.includes('Harry Potter')) {
+        text[i].innerHTML = text[i].innerHTML.replace('Harry Potter', (match) => '<span style="color: #000000; background-color: #000000">Harry Potter</span>')
+    }
+}
 
 /*for (let i = 0; i < text.length; i++) {
     if (text[i].innerHTML.includes('Harry Potter')) {
         text[i].innerHTML = text[i].innerHTML.replace('Harry Potter', 'Hairy Potter is very bad')
     }
 }*/
-for (let i = 0; i < text.length; i++) {
-    if (text[i].innerHTML.includes('Harry Potter')) {
-        let innerHTML = text[i].innerHTML;
-        let index = innerHTML.indexOf('Harry Potter');
-        if (index >= 0) {
-            innerHTML = innerHTML.substring(0,index) + "<span class='spoiler'>" + innerHTML.substring(index,index+'Harry Potter'.length) + "</span>" + innerHTML.substring(index + 'Harry Potter'.length);
-            text[i].innerHTML = innerHTML;
-        }
-    }
-}
 
 //document.body.innerHTML = ' ';
