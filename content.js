dac32a6e27c9f732b81e8796f9305f3537b5d16f
mpurushothapu3 <55211137+mpurushothapu3@users.noreply.github.com>
2020-09-05 21:24:25 -0400

    Update regex

--- content.js
+++ content.js
@@ -5,10 +5,9 @@
 
 searchText2 = config;
 
-text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
-regex = new RegExp(searchText2, 'gim')
-for (let i = 0; i < text.length; i++) {
-    if (text[i].innerHTML.includes(searchText2)) {
-        text[i].innerHTML = text[i].innerHTML.replace(regex, '<mark style="background-color: black">'+searchText2+'</mark>')
-    }
-}
+text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
+regex = new RegExp("(>[^><]*?)(" + searchText2 + ")([^><]*?</)", 'gim');
+
+
+document.body.innerHTML = document.body.innerHTML.replace(regex, '$1 <mark style="background-color: black">'+searchText2+'</mark> $3');
+
