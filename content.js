a9d775db020eba02e410629bc1bcc84ffbe723ec
VinnieKhanna <57320972+VinnieKhanna@users.noreply.github.com>
2020-09-06 04:06:04 -0400

    update

--- content.js
+++ content.js
@@ -1,12 +1,23 @@
 
 
+var instances = 0;
 searchText2 = config;
-// console.log(config);
 stringArray = searchText2.split(" ");
 text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
 
-stringArray.forEach((str) => {
+for (let i = 0; i < stringArray.length; i++) {
+
+    let str = stringArray[i];
 regex = new RegExp("(>[^><]*?)(" + str + ")([^><]*?<)", 'gim');
+
+instances += document.body.innerHTML.match(regex).length;
+
+
 document.body.innerHTML = document.body.innerHTML.replace(regex,
-    '$1 <mark style="background-color: black">'+str+'</mark> $3');
-});
+    '$1 <mark style="background-color: black" onclick="this.style.backgroundColor= \'transparent\'">'+str+'</mark> $3');
+
+
+}
+
+chrome.storage.sync.set({'instance': instances}, () => console.log(instances));
+
