876e06dc8fdfd5f439f66be70832de01b6d14c22
mpurushothapu3 <55211137+mpurushothapu3@users.noreply.github.com>
2020-09-05 21:41:26 -0400

    Update content.js

--- content.js
+++ content.js
@@ -6,7 +6,7 @@
 searchText2 = config;
 
 text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
-regex = new RegExp("(>[^><]*?)(" + searchText2 + ")([^><]*?</)", 'gim');
+regex = new RegExp("(>[^><]*?)(" + searchText2 + ")([^><]*?<)", 'gim');
 
 
 document.body.innerHTML = document.body.innerHTML.replace(regex, '$1 <mark style="background-color: black">'+searchText2+'</mark> $3');
